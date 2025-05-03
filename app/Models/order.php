970ec60c27fa50b6a8116ede95418d224c789ee4
kpp098 <patelkashyapp027@gmail.com>
2025-05-03 15:51:22 +0530

    minor error fix

--- app/Models/order.php
+++ app/Models/order.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class order extends Model
+class Order extends Model
 {
     protected $fillable = ['name', 'email', 'phone', 'amount', 'address', 'status', 'transaction_id', 'currency'];
 }
---
+++ database/migrations/2025_05_03_152513_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone')->nullable();
+            $table->decimal('amount', 10, 2);
+            $table->text('address')->nullable();
+            $table->string('status');
+            $table->string('transaction_id')->nullable();
+            $table->string('currency', 10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
